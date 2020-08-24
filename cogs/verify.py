9e0e63c77029592999bd263058deb33007d7032d
Isabella Szemeti <issythegirl@gmail.com>
2020-08-24 03:45:10 +0100

    fix(verify cog): Removed bare exceptions from task loop

--- cogs/verify.py
+++ cogs/verify.py
@@ -354,25 +354,33 @@ class Verify(commands.Cog):
 
     # Tasks
 
-    @tasks.loop()
+    # @tasks.loop()
+    # async def monitor_db(self):
+    #     """Monitor DB for changes"""
+    #     try:
+    #         logging.info("Monitoring DB")
+    #         async for change in db.queue.watch():
+    #             if change["operationType"] == "insert":
+    #                 user = await db.users.find_one({"_id": change["fullDocument"]["ref"]})
+
+    #                 if user.get("verified"):
+    #                     await self.set_verified(user['discord']['id'])
+
+    #                     await db.queue.find_one_and_delete({"_id": change["fullDocument"]['_id']})
+
+    #     except Exception as e:
+    #         logging.error(f'ERROR MONITORING DB: {e}')
+    #         logging.warning('WAITING BEFORE TRYING AGAIN')
+    #         time.sleep(5)
+
+    @tasks.loop(seconds=20)
     async def monitor_db(self):
         """Monitor DB for changes"""
-        try:
-            logging.info("Monitoring DB")
-            async for change in db.queue.watch():
-                if change["operationType"] == "insert":
-                    user = await db.users.find_one({"_id": change["fullDocument"]["ref"]})
-
-                    if user.get("verified"):
-                        await self.set_verified(user['discord']['id'])
-
-                        await db.queue.find_one_and_delete({"_id": change["fullDocument"]['_id']})
-
-        except Exception as e:
-            logging.error(f'ERROR MONITORING DB: {e}')
-            logging.warning('WAITING BEFORE TRYING AGAIN')
-            time.sleep(5)
-
+        logging.info("Monitoring DB")
+        queue = [i for i in db.queue.find()]
+        for user in [i for i in queue if i.get("verified")]:
+            await self.set_verified(user['discord']['id'])
+            await db.queue.find_one_and_delete(user["_id"])
 
 def setup(client):
     client.add_cog(Verify(client))
