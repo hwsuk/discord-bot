6b620891bd155adc77079e0e6b4744ed08d4c451
Isabella Szemeti <issythegirl@gmail.com>
2020-10-25 18:21:37 +0000

    chore(ebay): Comments

--- cogs/ebay.py
+++ cogs/ebay.py
@@ -22,10 +22,12 @@ class Ebay(commands.Cog):
         Usage example:
         !check dell xps 15 -dead -broken -scratched
         words beginning with `-` are added to the list of words to be filtered out"""
+        # If the searchterm with boolean operators removed is less than 6 characters
         if len(' '.join([i for i in searchTerm.split(' ') if not i.startswith('-')])) < 6:
             embed = await self.error_embed('length')
             await ctx.send(embed=embed)
             return
+
         async with ctx.channel.typing():
             filteredTerm, filteredWords = await self.get_filter(searchTerm)
             page = await self.make_soup(filteredTerm)
@@ -37,27 +39,33 @@ class Ebay(commands.Cog):
                     products.append(p)
 
             def filtered_out(title: str, filteredWords: list):
+                """Checks whether any words from the filter are in the listing title"""
                 for word in title.split(' '):
                     if word.lower() in filteredWords:
                         return True
                 return False
 
+            # Removes listings that don't pass the filter
             filteredListings = [i for i in products if filtered_out(i['title'], filteredWords) is False]
-            if len(filteredListings) == 0:
+            if not filteredListings: # If no listings pass filter
                 desc = [f"No results found for `{filteredTerm}`"]
                 desc.append("Make sure to include manufacturer for best results")
-                desc.append("If you feel this is in error, message Issy")
+                desc.append("If you feel this is in error, message one of the bot developers")
                 embed = discord.Embed(title='No results found', colour=0xE53238, description='\n'.join(desc))
                 fW = [i[1:] for i in searchTerm.split(' ') if i.startswith('-')]
-                if len(fW) > 0:
+                if fW:
                     embed.add_field(name="Filtered words", value='\n'.join(fW), inline=True)
                 await ctx.send(embed=embed)
                 return
+
+            # If there are listings that passed the filter
+            # Get the 25th, 50th and 75th percentiles of the price range
             quartiles = await self.find_quartiles([i['price'] for i in filteredListings])
-            # from 25th to 75th percentile
             boxPlot = [i['price'] for i in filteredListings if quartiles[0] <= i['price'] <= quartiles [2]]
             average = sum(boxPlot) / len(boxPlot)
+            # Get variance
             variance = await self.determine_variance(boxPlot)
+            # Make and send the embed
             embedDetails = {"title": f"Results for {filteredTerm}",
                             "range": [quartiles[0], quartiles[2]],
                             "median": quartiles[1],
@@ -69,20 +77,25 @@ class Ebay(commands.Cog):
             await ctx.send(embed=embed)
 
     async def make_embed(self, data:dict):
+        """Generate an embed object"""
         colour = await self.get_colour(data)
+
         def price(num):
-            return '%.2f' % num if num == int(num) or num < 1 else int(num)
+            """Formats a number as a price"""
+            return '%.2f' % num if round(num, 2) == int(num) or num < 1 else int(num)
+
         embed = discord.Embed(title=data['title'], colour=colour)
         embed.add_field(name='Range', value=f"£{price(data['range'][0])} - £{price(data['range'][1])}", inline=True)
         embed.add_field(name='Median', value=f"£{price(data['median'])}", inline=True)
         embed.add_field(name='Average', value=f"£{price(data['average'])}", inline=True)
         embed.add_field(name='Variance', value=f"{data['variance']}%", inline=True)
         embed.add_field(name='Number of items', value=data['numOfItems'], inline=True)
-        if data['filteredWords'] != []:
+        if data['filteredWords']:
             embed.add_field(name="Filtered words", value='\n'.join(data['filteredWords']), inline=False)
         return embed
 
     async def make_soup(self, searchTerm):
+        """Returns a BeautifulSoup object from an ebay search"""
         cleanSearchTerm = urllib.parse.quote(searchTerm) # clean up the search term for the url
         url = f"https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={searchTerm}&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1&_ipg=200"
         async with aiohttp.ClientSession() as session:
@@ -125,6 +138,7 @@ class Ebay(commands.Cog):
         return ' '.join(filteredSearchTerm), words
 
     async def get_product_info(self, product):
+        """Fetches the product info from a product listing as a dict"""
         try:
             d = {}
             d['title'] = product.find('h3', {'class': 's-item__title'}).contents[-1]
@@ -133,38 +147,65 @@ class Ebay(commands.Cog):
             d['price'] = await self.parse_price(product)
             d['ended_date'] = await self.parse_date(product)
             d['id'] = d['url'].split('/')[-1].split('?')[0]
+            # Type enforcement on dictionary object
             return {'title': str(d['title']), 'url': str(d['url']), 'image': str(d['image']), 'price': d['price'], 'ended_date': d['ended_date'], 'id': str(d['id'])}
         except:
             return None
 
     async def parse_price(self, product):
+        """Retrieves the full price from a product listing"""
         logReg = re.compile('\+\s£(\d+\.\d+).*')
-        basePrice = float(product.find('span', {'class': 's-item__price'}).contents[0].contents[0].strip('£').replace(',',''))
-        if product.find('span', {'class': 's-item__logisticsCost'}) is None:
+        basePrice = float(product.find('span', {'class': 's-item__price'}).contents[0].contents[0].strip('£').replace(',','')) # Find the listing price
+        if not product.find('span', {'class': 's-item__logisticsCost'}): # If there is no postage price return the listing price
             return basePrice
-        logistics = product.find('span', {'class': 's-item__logisticsCost'}).contents[0]
+        logistics = product.find('span', {'class': 's-item__logisticsCost'}).contents[0] 
         if type(logistics) is bs4.element.NavigableString:
             if logistics.lower() in ['free postage', 'postage not specified']:
                 postage = 0
             elif logReg.match(logistics):
                 postage = float(logReg.match(logistics).group(1))
-        else:
+        else: # If 
             logistics = logistics.contents[0]
             if logReg.match(logistics):
                 postage = float(logReg.match(logistics).group(1))
         return basePrice + postage
 
     async def parse_date(self, product):
+        """Retrieves the ended date from a product listing"""
+        # Fetch the date string
         base = product.find('span', {'class': 's-item__ended-date'}).contents[0]
+        # Retrieve the individual parts of the date
         day = int(base.split('-')[0])
         months = {datetime.date(2020, i, 1).strftime('%B')[:3]: i for i in range(1,13)}
         month = months[base.split('-')[1].split(' ')[0]]
         time = base.split(' ')[1]
         hour = int(time.split(':')[0])
         minute = int(time.split(':')[1])
+        # Convert to a datetime object
         return datetime.datetime(year=datetime.datetime.now().year, month=month, day=day, hour=hour, minute=minute)
 
+    async def find_quartiles(self, numArray: list):
+        """Find quartile positions from a list of prices"""
+        size = len(numArray)
+        if size == 0:
+            quartiles = [0,0,0]
+        else:
+            sortedArray = sorted(numArray)
+            lowerPos, medianPos, upperPos = await self.find_quartile_postions(size)
+            # Floor so can work in arrays
+            flooredLowerPos = math.floor(lowerPos)
+            flooredMedianPos = math.floor(medianPos)
+            flooredUpperPos = math.floor(upperPos)
+            # If position is an integer, the quartile is the elem at position
+            # else the quartile is the mean of the elem & the elem one position above
+            lowerQuartile = sortedArray[flooredLowerPos] if (lowerPos % 1 == 0) else (sortedArray[flooredLowerPos] + sortedArray[flooredLowerPos + 1]) / 2
+            median = sortedArray[flooredMedianPos] if (medianPos % 1 == 0) else (sortedArray[flooredMedianPos] + sortedArray[flooredMedianPos + 1]) / 2
+            upperQuartile = sortedArray[flooredUpperPos] if (upperPos % 1 == 0) else (sortedArray[flooredUpperPos] + sortedArray[flooredUpperPos + 1]) / 2
+            quartiles = [round(i, 2) for i in [lowerQuartile, median, upperQuartile]]
+        return quartiles
+
     async def find_quartile_postions(self, size: int):
+        """Determines quartile positions from """
         if size == 1:
             # All quartiles are the first (only) element
             return 0, 0, 0
@@ -176,32 +217,15 @@ class Ebay(commands.Cog):
             # Quartiles can be between positions if size + 1 is not divisible by 4
             return (size + 1) / 4 - 1, (size + 1) / 2 - 1, 3 * (size + 1) / 4 - 1
 
-    async def find_quartiles(self, num_array: list):
-        size = len(num_array)
-        if size == 0:
-            quartiles = [0,0,0]
-        else:
-            sorted_array = sorted(num_array)
-            lower_pos, median_pos, upper_pos = await self.find_quartile_postions(size)
-            # Floor so can work in arrays
-            floored_lower_pos = math.floor(lower_pos)
-            floored_median_pos = math.floor(median_pos)
-            floored_upper_pos = math.floor(upper_pos)
-            # If position is an integer, the quartile is the elem at position
-            # else the quartile is the mean of the elem & the elem one position above
-            lower_quartile = sorted_array[floored_lower_pos] if (lower_pos % 1 == 0) else (sorted_array[floored_lower_pos] + sorted_array[floored_lower_pos + 1]) / 2
-            median = sorted_array[floored_median_pos] if (median_pos % 1 == 0) else (sorted_array[floored_median_pos] + sorted_array[floored_median_pos + 1]) / 2
-            upper_quartile = sorted_array[floored_upper_pos] if (upper_pos % 1 == 0) else (sorted_array[floored_upper_pos] + sorted_array[floored_upper_pos + 1]) / 2
-            quartiles = [round(i, 2) for i in [lower_quartile, median, upper_quartile]]
-        return quartiles
-
     async def determine_variance(self, priceRange):
-        mean = sum(priceRange) / len(priceRange)
+        """Get the variance of a price range"""
+        mean = sum(priceRange) / len(priceRange) # Get the mean price
         priceRange.sort()
-        variance = (priceRange[-1] - priceRange[0]) / mean * 100
+        variance = (priceRange[-1] - priceRange[0]) / mean * 100 # Range over mean as a percentage
         return round(variance, 2)
 
     async def get_colour(self, data):
+        """Determines embed colour based on variance amount"""
         if 0 <= data['variance'] < 16.6:
             return 0x78B159 # green
         elif 16.6 <= data['variance'] < 33.3:
@@ -210,6 +234,7 @@ class Ebay(commands.Cog):
             return 0xDD2E44 # red
 
     async def error_embed(self, error):
+        """Generates an error embed"""
         errors = {'length': 'Your search must be more than 6 characters',
                   'timeout': 'The server failed to fetch a result'}
         return discord.Embed(title='Error', description=errors[error], colour=0xDD2E44)
