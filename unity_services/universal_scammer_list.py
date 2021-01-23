c602ffd0b62082433097a0e4baec1aee901b8229
Toby Jones <github@monotron.me>
2021-01-23 17:34:54 +0000

    Implement USL cog

--- unity_services/universal_scammer_list.py
+++ unity_services/universal_scammer_list.py
@@ -1,15 +1,18 @@
 import httpx
 
-from bs4 import BeautifulSoup
-
 usl_query_base_url = "https://universalscammerlist.com/api/query.php?query={}&hashtags=%23scammer%2C%23sketchy%2C%23troll&format=1"
 
 
 async def fetch_usl_user_data(username):
     async with httpx.AsyncClient() as client:
-        response = client.get(usl_query_base_url.format(username))
+        response = await client.get(usl_query_base_url.format(username))
 
     if response.status_code != 200:
         raise RuntimeError(f"USL request failed: HTTP {response.status_code}")
 
-    data = response.json()
+    json = response.json()
+
+    if not "data" in json:
+        raise RuntimeError("Invalid response from USL API: no 'data' key")
+
+    return json["data"]
