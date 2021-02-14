e8d0d4dde12604a8cde16a0c5251109d27860192
Issy <isabella.szemeti@gmail.com>
2021-02-14 15:01:07 +0000

    chore(setup.py): Linting

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='unity_discord_bot',
-    packages=['unity_discord_bot'],
+    name="unity_discord_bot",
+    packages=["unity_discord_bot"],
     include_package_data=True,
-    install_requires=[]
+    install_requires=[],
 )
