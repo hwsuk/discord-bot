9cf511655e53508875e566c633bab22fc7dff073
Issy <isabella.szemeti@gmail.com>
2021-02-14 12:55:54 +0000

    feat(bot): Added central logger - removed role configs from source control

--- bot.py
+++ bot.py
@@ -1,17 +1,21 @@
-import discord
-from discord.ext import commands
 import json
 import logging
-from unity_util import bot_config
 import sys
 
-with open('config.json', 'r') as f:
-    conf = json.load(f)
+import discord
+from discord.ext import commands
 
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s]\t %(name)s: %(message)s", handlers=[
-    logging.StreamHandler(sys.stdout),
-    logging.FileHandler(f'./logs/{bot_config.LOGGING_FILENAME}')
-])
+from unity_util import bot_config
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s]\t %(name)s: %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(f"./logs/{bot_config.LOGGING_FILENAME}"),
+    ],
+)
 
 intents = discord.Intents(
     guilds=True,
@@ -23,38 +27,46 @@ intents = discord.Intents(
     dm_messages=True,
     reactions=True,
     guild_reactions=True,
-    dm_reactions=True
+    dm_reactions=True,
 )
-client = commands.Bot(command_prefix = bot_config.DISCORD_PREFIX, intents=intents)
+client = commands.Bot(
+    command_prefix=bot_config.DISCORD_PREFIX, intents=intents)
+
 
 @client.event
 async def on_ready():
-    logging.info(f'Logged in as {client.user.name}, {client.user.id}')
-    logging.info('-----------------------------------------')
+    logging.info(f"Logged in as {client.user.name}, {client.user.id}")
+    logging.info("-----------------------------------------")
     await client.change_presence(activity=discord.Game("Verifying 👀"))
 
-@client.command()
-@commands.is_owner()
-async def load(ctx, extension):
-    client.load_extension(f'unity_cogs.{extension}')
-    await ctx.send(f'{extension} loaded')
-    logging.info(f'{extension} loaded')
 
 @client.command()
 @commands.is_owner()
-async def unload(ctx, extension):
-    client.unload_extension(f'unity_cogs.{extension}')
-    await ctx.send(f'{extension} unloaded')
-    logging.info(f'{extension} unloaded')
+async def load(ctx, extension: str):
+    client.load_extension(f"unity_cogs.{extension}")
+    await ctx.send(f"{extension} loaded")
+    logging.info(f"{extension} loaded")
+
 
 @client.command()
 @commands.is_owner()
-async def reload(ctx, extension):
-    client.reload_extension(f'unity_cogs.{extension}')
-    await ctx.send('{} reloaded'.format(extension))
-    logging.info(f'{extension} reloaded')
+async def unload(ctx, extension: str):
+    client.unload_extension(f"unity_cogs.{extension}")
+    await ctx.send(f"{extension} unloaded")
+    logging.info(f"{extension} unloaded")
 
-for cog in conf['preloaded']:
-    client.load_extension(f'unity_cogs.{cog}')
+
+@client.command()
+@commands.is_owner()
+async def reload(ctx, extension: str):
+    client.reload_extension(f"unity_cogs.{extension}")
+    await ctx.send(f"{extension} reloaded")
+    logging.info(f"{extension} reloaded")
+
+
+with open("cogs.json", "r") as f:
+    cogs = json.loads(f.read())
+    for cog in cogs:
+        client.load_extension(f"unity_cogs.{cog}")
 
 client.run(bot_config.DISCORD_TOKEN)
